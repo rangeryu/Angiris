8cd0ce2ab3006dfb8f2613837b81850107b0cb45
Rocco Ren <me@roccoren.com>
2015-04-02 17:03:06 +0800

    Add Sms/Email Delivery Service

---
+++ src/Angiris.CentralAdmin.WebApp/Controllers/ApiController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using RestSharp;
+using RestSharp.Extensions;
+using RestSharp.Validation;
+
+namespace Angiris.CentralAdmin.WebApp.Controllers
+{
+    public class ApiController
+    {
+        private static readonly string _secretKey = "key-c71cc13573773b936e20883d961dcb84";
+
+        public static IRestResponse SendEmail(string To, string From, string Body, string Subject)
+        {
+            RestClient client = new RestClient();
+            client.BaseUrl = new Uri("https://api.mailgun.net/v2");
+            client.Authenticator =
+                    new HttpBasicAuthenticator("api",
+                                               _secretKey);
+            RestRequest request = new RestRequest();
+            request.AddParameter("domain",
+                                 "alephzero.cn", ParameterType.UrlSegment);
+            request.Resource = "{domain}/messages";
+            request.AddParameter("from", "No-Reply <no-reply@alephzero.cn>");
+            request.AddParameter("to", To);
+            request.AddParameter("subject", Subject);
+            request.AddParameter("text", Body);
+            request.AddParameter("o:tracking", true);
+            request.Method = Method.POST;
+            return client.Execute(request);
+        }
+    }
+}
