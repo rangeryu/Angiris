eed9ff0ea955efcb3356c46a742936d8b6959863
Rocco Ren <me@roccoren.com>
2015-04-25 00:20:18 +0800

    Add new template for status

---
+++ src/Angiris.CentralAdmin.WebApp/Scripts/Keys.js
@@ -0,0 +1,6 @@
+﻿var metadataQueueName = "blairtelemetryqueue";      // Create through Azure Portal
+var controlChannelQueueName = "blaircontrolqueue";   // Create through Azure Portal
+
+var eventHubNamespace = "blairiot";
+var sasKey = "4YrhMaMorrH98CehT8WREsfVXM2IXObAevlMWvpdabg=";
+var sasKeyName = "RootManageSharedAccessKey";
